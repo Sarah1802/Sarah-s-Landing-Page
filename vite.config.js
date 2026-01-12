fcb8db9addbd05a4654af25690b751fa779258c1
Sarah Liu <Sarah.Liu.1802@gmail.com>
2026-01-11 20:16:20 -0800

    changed icon image

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/Sarah-s-Landing-Page/",
+  plugins: [react()]
 })
